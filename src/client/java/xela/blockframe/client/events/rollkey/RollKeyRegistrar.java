604545bd7e6236c5a0f8cb7341e72bef2b65d1b8
alex <thegamerale97@gmail.com>
2026-05-21 21:11:37 +0200

    Fixed roll applying a y force to player

--- src/client/java/xela/blockframe/client/events/rollkey/RollKeyRegistrar.java
+++ src/client/java/xela/blockframe/client/events/rollkey/RollKeyRegistrar.java
@@ -5,13 +5,12 @@ import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.keymapping.v1.KeyMappingHelper;
 import net.fabricmc.fabric.api.client.networking.v1.ClientPlayNetworking;
 import net.minecraft.client.KeyMapping;
-import net.minecraft.world.phys.Vec3;
 import org.lwjgl.glfw.GLFW;
 import xela.blockframe.BlockFrame;
+import xela.blockframe.client.events.doublejumpkey.DoubleJumpRegistrar;
 import xela.blockframe.network.payloads.classes.VectorPayload;
 import xela.blockframe.network.payloads.records.ServerBoundMovementPayload;
 
-import static xela.blockframe.client.events.doublejumpkey.DoubleJumpRegistrar.CATEGORY;
 
 public class RollKeyRegistrar {
 
@@ -22,7 +21,7 @@ public class RollKeyRegistrar {
             InputConstants.Type.KEYSYM,
             GLFW.GLFW_KEY_LEFT_SHIFT,
             //Category can only be specified in 1 keybind file, else at startup it faisl to launch mc
-            CATEGORY
+            DoubleJumpRegistrar.CATEGORY
     ));
 
     private static int landingCooldown = 20;
@@ -37,13 +36,13 @@ public class RollKeyRegistrar {
                 landingCooldown = landingCooldown - 1;
             }else if ( landingCooldown <= 0){
                 while (Roll.consumeClick()) {
-                    //TODO: is this the sweetspot?
                     if (hasBeenPressed && ticksPassed < 15 && client.player != null) {
                         landingCooldown = 20;
                         var payload = new VectorPayload();
                         payload.UUID = client.player.getStringUUID();
                         var pushVec = client.player.getLookAngle();
-                        payload.pushVector = pushVec.add(new Vec3(0, 0.25, 0));
+                        //Void the y movement
+                        payload.pushVector = pushVec.add(0.2).add(0,-pushVec.y,0);
                         payload.typeof = "ROLL";
                         ClientPlayNetworking.send(new ServerBoundMovementPayload(payload));
                         BlockFrame.LOGGER.info(String.valueOf(ticksPassed));
