3f600641230c698aa2d9cf74beb6ef659bec33fc
alex <thegamerale97@gmail.com>
2026-06-03 16:58:50 +0200

    Add random effect application logic to `AttackedEvent` and update `EffectsRegistrar` with effect pool

--- src/main/java/xela/blockframe/effects/EffectsRegistrar.java
+++ src/main/java/xela/blockframe/effects/EffectsRegistrar.java
@@ -7,6 +7,8 @@ import net.minecraft.resources.Identifier;
 import net.minecraft.world.effect.MobEffect;
 import xela.blockframe.BlockFrame;
 
+import java.util.List;
+
 public class EffectsRegistrar {
     public static final Holder<MobEffect> SLASH = Registry.registerForHolder(BuiltInRegistries.MOB_EFFECT,
             Identifier.fromNamespaceAndPath(BlockFrame.MOD_ID, "slash"), new SlashEffect());
@@ -14,6 +16,8 @@ public class EffectsRegistrar {
     public static final Holder<MobEffect> COLD = Registry.registerForHolder(BuiltInRegistries.MOB_EFFECT,
             Identifier.fromNamespaceAndPath(BlockFrame.MOD_ID, "cold"), new ColdEffect());
 
+    //This will be used purely to get a random effects from the effect pool
+    public static final List<Holder<MobEffect>> EFFECTS = List.of(SLASH,COLD);
     public static void init(){
 
     }
--- src/main/java/xela/blockframe/events/AttackedEvent.java
+++ src/main/java/xela/blockframe/events/AttackedEvent.java
@@ -9,6 +9,8 @@ import net.minecraft.world.entity.player.Player;
 import xela.blockframe.BlockFrame;
 import xela.blockframe.effects.EffectsRegistrar;
 
+import java.util.Random;
+
 public class AttackedEvent {
     public static void attackEventRegistrar() {
         ServerLivingEntityEvents.AFTER_DAMAGE.register(AttackedEvent::registerAttackedEvent);
@@ -16,12 +18,18 @@ public class AttackedEvent {
 
     public static void registerAttackedEvent(LivingEntity entity, DamageSource source, float baseDamageTaken,
                                              float damageTaken, boolean blocked) {
+        Random rand = new Random();
         //TODO: damage by type of mob?
         if (entity instanceof Player player && source.getEntity() instanceof Entity) {
             //TODO: scale with server time / player level?
             if (Math.random() > (1 - BlockFrame.CONFIG.chanche_for_status_effects_to_apply()) && BlockFrame.CONFIG.should_roll_for_status_effects_on_player_damaged()){
-                player.addEffect(new MobEffectInstance(EffectsRegistrar.SLASH, 10*20, 1, true,
-                        true,true));
+                player.addEffect(new MobEffectInstance(
+                        EffectsRegistrar.EFFECTS.get(rand.nextInt(EffectsRegistrar.EFFECTS.size())),
+                        10*20,
+                        1,
+                        true,
+                        true,
+                        true));
             }
 
             /*
