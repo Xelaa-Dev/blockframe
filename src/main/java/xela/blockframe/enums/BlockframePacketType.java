f335e68e38f0b0dbc9d2a65fb3a7e96dbbdb369f
alex <thegamerale97@gmail.com>
2026-06-10 14:00:14 +0200

    Add double jump sound logic, packet type enum, and configuration option for toggling

---
+++ src/main/java/xela/blockframe/enums/BlockframePacketType.java
@@ -0,0 +1,13 @@
+package xela.blockframe.enums;
+
+public enum BlockframePacketType {
+    DOUBLE_JUMP("DOUBLE_JUMP");
+
+
+    public final String name;
+
+    private BlockframePacketType(String name){
+        this.name = name;
+    }
+
+}
--- src/main/java/xela/blockframe/network/payloads/handlers/MovementPacketsHandler.java
+++ src/main/java/xela/blockframe/network/payloads/handlers/MovementPacketsHandler.java
@@ -1,11 +1,18 @@
 package xela.blockframe.network.payloads.handlers;
 
+import net.minecraft.network.protocol.PacketType;
 import net.minecraft.network.protocol.game.ClientboundSetEntityMotionPacket;
+import net.minecraft.network.protocol.game.ClientboundSoundPacket;
+import net.minecraft.server.level.ServerLevel;
 import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.sounds.SoundEvent;
 import net.minecraft.sounds.SoundEvents;
+import net.minecraft.sounds.SoundSource;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.phys.Vec3;
+import xela.blockframe.BlockFrame;
 import xela.blockframe.effects.EffectsRegistrar;
+import xela.blockframe.enums.BlockframePacketType;
 import xela.blockframe.network.ChannelRegistrar;
 import xela.blockframe.network.payloads.records.MovementVectorPacket;
 
@@ -16,6 +23,7 @@ public class MovementPacketsHandler {
         ChannelRegistrar.SERVERBOUND_CHANNEL.registerServerbound(MovementVectorPacket.class, (message, access) ->{
             Vec3 newVec3;
             Entity entity = access.player().level().getEntity(UUID.fromString(message.UUID()));
+            ServerLevel level = access.runtime().getLevel(entity.level().dimension());
 
             if (entity instanceof ServerPlayer && !entity.level().isClientSide()){
 
@@ -27,16 +35,35 @@ public class MovementPacketsHandler {
                     newVec3 = message.pushVector().multiply(dampening,dampening,dampening);
 
                     entity.push(newVec3);
+
+
                 }else{
                     entity.push(message.pushVector());
                 }
 
 
+
                 //Get the player entity as a generic Entity class to get connection (?)
                 ((ServerPlayer)entity).connection.send(new ClientboundSetEntityMotionPacket(entity));
 
-                entity.playSound(SoundEvents.SAND_PLACE, 10.0F, 1.0F);
+                if (message.typeof() != null && message.typeof().equals(BlockframePacketType.DOUBLE_JUMP.name) &&
+                        BlockFrame.CONFIG.play_double_jump_sound()){
+                    ((ServerPlayer) entity).connection.send(new ClientboundSoundPacket(SoundEvents.ARMOR_EQUIP_WOLF, SoundSource.PLAYERS,
+                            entity.position().x,
+                            entity.position().y,
+                            entity.position().z,
+                            2f,
+                            1f,
+                            0
+                    ));
+                }
+                else {
+                    throw new RuntimeException("Unknown or malformed packet type");
+                }
             }
+            //Because of how sound is synced, it needs to be OUTSIDE ifs that include checking if we are on the server (nvm lol)
+            entity.playSound(SoundEvents.ARMOR_EQUIP_WOLF.value(), 2f,0.7f);
+
         });
     }
 }
