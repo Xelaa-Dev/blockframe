d1d5e87a98c673297fc19c14ec034055b03aa13e
alex <thegamerale97@gmail.com>
2026-06-16 09:52:07 +0200

    Forgot to remove debug logging from latest commit

--- src/main/java/xela/blockframe/mixin/ExampleMixin.java
+++ src/main/java/xela/blockframe/mixin/ExampleMixin.java
@@ -2,7 +2,6 @@ package xela.blockframe.mixin;
 
 import com.llamalad7.mixinextras.injector.ModifyReturnValue;
 import net.minecraft.world.entity.LivingEntity;
-import net.minecraft.world.entity.player.Player;
 import org.jspecify.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -11,13 +10,9 @@ import xela.blockframe.BlockFrame;
 
 @Mixin(LivingEntity.class)
 public abstract class ExampleMixin {
-	@Shadow
-	public abstract @Nullable LivingEntity asLivingEntity();
-
 	@ModifyReturnValue(method = "calculateFallDamage", at = @At(value = "TAIL"))
 	private int ignoreFallDamage(int original) {
 		if (!BlockFrame.CALC_FALL_DAMAGE){
-			BlockFrame.LOGGER.info("ticking mixin");
 			return 0;
 		}else if (BlockFrame.CALC_FALL_DAMAGE){
 			return original;
--- src/main/java/xela/blockframe/networking/payloads/handlers/GenericStringMessagePacketsHandler.java
+++ src/main/java/xela/blockframe/networking/payloads/handlers/GenericStringMessagePacketsHandler.java
@@ -1,6 +1,5 @@
 package xela.blockframe.networking.payloads.handlers;
 
-import com.jcraft.jorbis.Block;
 import io.wispforest.owo.network.ServerAccess;
 import xela.blockframe.BlockFrame;
 import xela.blockframe.networking.ChannelRegistrar;
@@ -14,12 +13,10 @@ public class GenericStringMessagePacketsHandler {
     private static void handler(GenericStringMessagePacket message, ServerAccess access) {
         switch (message.payload()){
             case "FALL_DAMAGE_DISABLE":
-                BlockFrame.LOGGER.info("Fall damage disabled");
                 BlockFrame.CALC_FALL_DAMAGE = false;
                 break;
 
             case "FALL_DAMAGE_ENABLE":
-                BlockFrame.LOGGER.info("Fall damage enabled");
                 BlockFrame.CALC_FALL_DAMAGE = true;
                 break;
 
