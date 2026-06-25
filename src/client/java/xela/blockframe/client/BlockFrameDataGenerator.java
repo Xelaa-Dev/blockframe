386f2451b41597e029d8bc89d4aa11802a6c2a60
alex <thegamerale97@gmail.com>
2026-06-25 15:29:17 +0200

    Remove unused `TagGen` class and its associated references from BlockFrameDataGenerator.

--- src/client/java/xela/blockframe/client/BlockFrameDataGenerator.java
+++ src/client/java/xela/blockframe/client/BlockFrameDataGenerator.java
@@ -6,7 +6,6 @@ import net.minecraft.core.RegistrySetBuilder;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.world.damagesource.DamageScaling;
 import net.minecraft.world.damagesource.DamageType;
-import xela.blockframe.client.datagen.TagGen;
 import xela.blockframe.client.datagen.TranslationsGen;
 import xela.blockframe.data.DamageSources;
 
@@ -16,18 +15,5 @@ public class BlockFrameDataGenerator implements DataGeneratorEntrypoint {
 	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
 		FabricDataGenerator.Pack pack = fabricDataGenerator.createPack();
 		pack.addProvider(TranslationsGen::new);
-		pack.addProvider(TagGen::new);
-	}
-
-	@Override
-	public void buildRegistry(RegistrySetBuilder registryBuilder) {
-		//Bootstrap the registry
-		registryBuilder.add(Registries.DAMAGE_TYPE, context -> {
-			context.register(
-					DamageSources.SLASH_DAMAGE,
-					//Match the already genned files for damagetypes
-					new DamageType("magic", DamageScaling.NEVER, 0)
-			);
-		});
 	}
 }
