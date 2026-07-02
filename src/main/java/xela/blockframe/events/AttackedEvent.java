c1548f3e65b9ecf4bc413f2c301f9cf1e80b97b1
alex <thegamerale97@gmail.com>
2026-07-02 12:16:00 +0200

    Implement TickEvent, refine damage type handling to reflect mob-specific damage (skeleton so far), and adjust armor bypass for Puncture effect

--- src/main/java/xela/blockframe/events/AttackedEvent.java
+++ src/main/java/xela/blockframe/events/AttackedEvent.java
@@ -1,9 +1,12 @@
 package xela.blockframe.events;
 
 import net.fabricmc.fabric.api.entity.event.v1.ServerLivingEntityEvents;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.tags.EntityTypeTags;
 import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
@@ -11,13 +14,14 @@ import xela.blockframe.BlockFrame;
 import xela.blockframe.effects.EffectsRegistrar;
 
 import java.util.Random;
+import java.util.function.Predicate;
 
 public class AttackedEvent {
     public static void attackEventRegistrar() {
-        ServerLivingEntityEvents.AFTER_DAMAGE.register(AttackedEvent::TickApplyRandomEffect);
         ServerLivingEntityEvents.AFTER_DAMAGE.register(AttackedEvent::TickCheckTypeofDamage);
     }
 
+    @Deprecated(since = "Mobs will each have its own damage type accordingly")
     private static void TickApplyRandomEffect(LivingEntity entity, DamageSource source, float baseDamageTaken,
                                              float damageTaken, boolean blocked) {
         Random rand = new Random();
@@ -34,14 +38,26 @@ public class AttackedEvent {
         }
     }
 
+    /*
+    Logically the first arrow that hits will not have a puncture effect. for now it will stay like this (see below)
+    TODO: predict the hit from a arrow
+     */
     private static void TickCheckTypeofDamage(Entity entity, DamageSource source, float baseDamageTaken,
                                              float damageTaken, boolean blocked) {
         if (entity instanceof Player player && source.getEntity() instanceof Entity attacker) {
-            BlockFrame.LOGGER.info(String.valueOf(source.getEntity()));
-
-            source.getEntity().getName();
             var type = attacker.getType();
-            BlockFrame.LOGGER.info(String.valueOf(source.getEntity().getType()));
+            //TODO: adapt as a switch statement
+
+            if (type.equals(EntityType.SKELETON) && !player.hasEffect(EffectsRegistrar.PUNCTURE)) {
+                player.addEffect(new MobEffectInstance(
+                        EffectsRegistrar.PUNCTURE,
+                        10*20,
+                        1,
+                        true,
+                        true,
+                        true));
+            }
+
         }
     }
 }
