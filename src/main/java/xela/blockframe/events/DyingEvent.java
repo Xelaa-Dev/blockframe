e2108950a3c127cc453701cd0fa17f4e55ebd00d
alex <thegamerale97@gmail.com>
2026-08-29 13:24:18 +0200

    [26.2] Added a map to track when players have the cold render enabled

--- src/main/java/xela/blockframe/events/DyingEvent.java
+++ src/main/java/xela/blockframe/events/DyingEvent.java
@@ -1,22 +1,30 @@
 package xela.blockframe.events;
 
 import net.fabricmc.fabric.api.entity.event.v1.ServerLivingEntityEvents;
+import net.fabricmc.fabric.api.entity.event.v1.ServerPlayerEvents;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.entity.LivingEntity;
+import xela.blockframe.BlockFrame;
+import xela.blockframe.networking.ChannelRegistrar;
+import xela.blockframe.networking.payloads.records.EffectEndPayload;
 
 public class DyingEvent {
     public static void init() {
-        ServerLivingEntityEvents.AFTER_DEATH.register(DyingEvent::onEntityDeath);
+        ServerPlayerEvents.AFTER_RESPAWN.register(DyingEvent::onEntityDeath);
     }
 
-    private static void onEntityDeath(LivingEntity livingEntity, DamageSource damageSource) {
-        if (livingEntity instanceof ServerPlayer player) {
-            if (player.level().dimension() == ServerLevel.OVERWORLD) {
 
+    private static void onEntityDeath(ServerPlayer player, ServerPlayer serverPlayer1, boolean b) {
+
+            if (EffectFinishingEvent.effectRendererMap.containsKey(player.getUUID()) &&
+                    EffectFinishingEvent.effectRendererMap.get(player.getUUID())) {
+
+                BlockFrame.LOGGER.info("Forcing render disable for " + player.getName().getString());
+                ChannelRegistrar.NET_CHANNEL.serverHandle(player).
+                        send(new EffectEndPayload("COLD", "DISABLE_RENDER"));
             }
-        }
     }
 
 
--- src/main/java/xela/blockframe/events/EffectFinishingEvent.java
+++ src/main/java/xela/blockframe/events/EffectFinishingEvent.java
@@ -6,21 +6,25 @@ import xela.blockframe.effects.EffectsRegistrar;
 import xela.blockframe.networking.ChannelRegistrar;
 import xela.blockframe.networking.payloads.records.EffectEndPayload;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
 /*
 TODO:Used the disk data setup to write to disk when the renderer is enabled, by logic if th eplayer dies
     and the data says his renderer is still on then we need to send a packet on death to disable it
 */
 
 public class EffectFinishingEvent {
+    public static Map<UUID, Boolean> effectRendererMap = new HashMap<>();
 
     public static void registerEffectFinishingEvent() {
         ServerMobEffectEvents.BEFORE_REMOVE.register((effectInstance, entity, ctx) -> {
             if (effectInstance.is(EffectsRegistrar.COLD)) {
                 var player = (Player) entity;
                 //FROM the server to the client
-
-
                 ChannelRegistrar.NET_CHANNEL.serverHandle(player).send(new EffectEndPayload("COLD", "DISABLE_RENDER"));
+                effectRendererMap.put(player.getUUID(), false);
             }
         });
     }
@@ -30,8 +34,8 @@ public class EffectFinishingEvent {
             if (effectInstance.is(EffectsRegistrar.COLD)) {
                 var player = (Player) entity;
                 //FROM the server to the client
-
                 ChannelRegistrar.NET_CHANNEL.serverHandle(player).send(new EffectEndPayload("COLD", "ENABLE_RENDER"));
+                effectRendererMap.put(player.getUUID(), true);
             }
         });
     }
