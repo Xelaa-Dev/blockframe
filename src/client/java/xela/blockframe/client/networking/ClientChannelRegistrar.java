a59e3201da87199286ca2b7aac858c11bbb7247e
alex <thegamerale97@gmail.com>
2026-09-19 17:37:55 +0200

    [26.1] Still working on it

--- src/client/java/xela/blockframe/client/networking/ClientChannelRegistrar.java
+++ src/client/java/xela/blockframe/client/networking/ClientChannelRegistrar.java
@@ -1,12 +1,14 @@
 package xela.blockframe.client.networking;
 
 import io.wispforest.owo.ui.component.LabelComponent;
+import io.wispforest.owo.ui.container.FlowLayout;
 import io.wispforest.owo.ui.hud.Hud;
 import net.minecraft.client.ComponentCollector;
 import net.minecraft.network.chat.Component;
 import xela.blockframe.BlockFrame;
 import xela.blockframe.client.BlockFrameClient;
 import xela.blockframe.networking.ChannelRegistrar;
+import xela.blockframe.networking.payloads.records.HudUpdatePayload;
 
 public class ClientChannelRegistrar {
     public static void init() {
@@ -23,22 +25,32 @@ public class ClientChannelRegistrar {
         });
 
         ChannelRegistrar.NET_CHANNEL.registerClientbound(xela.blockframe.networking.payloads.records.HudUpdatePayload.class, (message, access) -> {
-           var component = Hud.getComponent(message.ID());
-            if (component == null){
-                BlockFrame.LOGGER.warn("component is null");
-            }else {
-                //Cast to label
-                var castedComponent = (LabelComponent) component;
-                var original = Integer.parseInt(castedComponent.text().getString());
-                switch (message.operation()) {
-                    case HUD_DECREASE_STACK:
-                        castedComponent.text(Component.empty().append(String.valueOf(original - 1)));
-                        break;
-                    case HUD_INCREMENT_STACK:
-                        castedComponent.text(Component.empty().append(String.valueOf(original + 1)));
-                        break;
-                }
-            }
+            refreshUi(message);
         });
+    }
+
+    private static void refreshUi(HudUpdatePayload message) {
+        var component = Hud.getComponent(message.ID());
+        if (component == null){
+            BlockFrame.LOGGER.warn("component is null");
+        }else {
+            //Cast to label
+            var castedComponent = (FlowLayout) component;
+
+            for (var child :  castedComponent.children()) {
+
+            }
+            /*
+            var original = Integer.parseInt(castedComponent.text().getString());
+            switch (message.operation()) {
+                case HUD_DECREASE_STACK:
+                    castedComponent.text(Component.empty().append(String.valueOf(original - 1)));
+                    break;
+                case HUD_INCREMENT_STACK:
+                    castedComponent.text(Component.empty().append(String.valueOf(original + 1)));
+                    break;
+            }
+            */
+        }
     }
 }
