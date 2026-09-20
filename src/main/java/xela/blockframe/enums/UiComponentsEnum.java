b68f0a944052bd8be666253e4e1d5ca38bf2b728
alex <thegamerale97@gmail.com>
2026-09-20 11:05:52 +0200

    [26.2] More HUD work

---
+++ src/main/java/xela/blockframe/enums/UiComponentsEnum.java
@@ -0,0 +1,19 @@
+package xela.blockframe.enums;
+
+import net.minecraft.resources.Identifier;
+
+public enum UiComponentsEnum {
+    //These are supposed to identify the UI components for effects, the actual number is gonna be the using the
+    // identifier + "_value" id style
+    COLD(Identifier.fromNamespaceAndPath("blockframe", "ui_cold")),
+    IMPACT(Identifier.fromNamespaceAndPath("blockframe", "ui_impact")),
+    PUNCTURE(Identifier.fromNamespaceAndPath("blockframe", "ui_puncture")),
+    SLASH(Identifier.fromNamespaceAndPath("blockframe", "ui_slash"));
+
+
+    public final Identifier name;
+
+    private UiComponentsEnum(Identifier name) {
+        this.name = name;
+    }
+}
--- src/main/java/xela/blockframe/networking/payloads/handlers/HudUpdatePacketHandler.java
+++ src/main/java/xela/blockframe/networking/payloads/handlers/HudUpdatePacketHandler.java
@@ -7,8 +7,8 @@ import xela.blockframe.networking.ChannelRegistrar;
 import xela.blockframe.networking.payloads.records.HudUpdatePayload;
 
 public class HudUpdatePacketHandler {
-    public static void registerHudUpdatePacketHandler(Player player, BlockframePacketType operation, int amount) {
-        ChannelRegistrar.NET_CHANNEL.serverHandle(player).send(new HudUpdatePayload(Identifier.fromNamespaceAndPath("blockframe", "ui")
+    public static void registerHudUpdatePacketHandler(Player player, BlockframePacketType operation, int amount, Identifier hudElementId) {
+        ChannelRegistrar.NET_CHANNEL.serverHandle(player).send(new HudUpdatePayload(hudElementId
         ,operation, amount));
     }
 }
